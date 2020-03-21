e48eb919aaf91cff61a24adbe6939ef856d5340a
Sam Thorogood <sam.thorogood@gmail.com>
2020-03-21 12:12:14 +1100

    test percy nav

--- tools/percy/snapshots.js
+++ tools/percy/snapshots.js
@@ -1,5 +1,9 @@
 const PercyScript = require("@percy/script");
 const scrollToBottom = require("scroll-to-bottomjs");
+const puppeteer = require('puppeteer');  // for types only
+
+const isDev = Boolean(process.env.WEBDEV_PERCY_DEV)  // set by snapshots-dev.js
+
 const pagesToTest = [
   {
     url: "",
@@ -25,15 +29,23 @@ const pagesToTest = [
 
 // A script to navigate our app and take snapshots with Percy.
 PercyScript.run(
-  async (browser, percySnapshot) => {
-    for (page of pagesToTest) {
-      await browser.goto(`http://localhost:8080/${page.url}`);
-      await browser.evaluate(scrollToBottom);
-      await browser.waitFor(2000);
-      await percySnapshot(`${page.title}`);
+  async (/** @type {!puppeteer.Page} */ page, percySnapshot) => {
+    for (const {url, title} of pagesToTest) {
+      await page.goto(`http://localhost:8080/${url}`);
+      await page.evaluate(scrollToBottom);
+      await page.waitFor(2000);
+      await percySnapshot(title);
     }
+    // Navigate back to home page by clicking on a link. We expect most assets
+    // to already be lazyloaded by this point so don't scroll.
+    // This is a canary for problems with the SPA routing code.
+    const navigationPromise = page.waitForNavigation();
+    await page.click(".web-header__logo-link");
+    await navigationPromise;
+    await page.waitFor(200);
+    await percySnapshot("Home page, navigation");
   },
   // These flags remove Percy's single-process flag. Without these, any page
   // with an iframe will crash puppeteer (and percy).
-  {args: ["--no-sandbox", "--disable-setuid-sandbox"]},
+  {headless: !isDev, args: ["--no-sandbox", "--disable-setuid-sandbox"]},
 );
